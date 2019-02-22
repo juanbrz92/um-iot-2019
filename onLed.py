f87deacca83b9eb8eb0454fe33d1239d641ae4e0
Juan Brizuela <juanbrz92@hotmail.com>
2019-02-21 21:55:19 -0300

    Update onLed.py

--- onLed.py
+++ onLed.py
@@ -1,27 +1,10 @@
-import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
-
-def on_connect(client, userdata, rc):
-    #print ("Connected with rc: " + str(rc))
-    client.subscribe("dht11sensor/DHT11")
-
-def on_message(client, userdata, msg):
-    #print ("Topic: "+ msg.topic+"\nMessage: "+str(msg.payload))
-    if TRUE in msg.payload:
-        #print("  Red on!")
-        GPIO.output(13, True)
-    else:
-        #print("  Red off!")
-        GPIO.output(13, False)
-        
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect("test.mosquitto.org", 1883, 60)
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.OUT)
-
-client.loop_forever()
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep # Import the sleep function from the time module
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+while True: # Run forever
+ GPIO.output(8, GPIO.HIGH) # Turn on
+ sleep(1) # Sleep for 1 second
+ GPIO.output(8, GPIO.LOW) # Turn off
+ sleep(1) # Sleep for 1 second
